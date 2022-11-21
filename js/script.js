dd472e102afcf7f9ab5e50501460597b69ffa863
Tengyyy <hansmartintomingas@gmail.com>
2022-11-22 00:29:37 +0200

    Avalehe esmane versioon

--- js/script.js
+++ js/script.js
@@ -37,3 +37,33 @@ window.onload = () =>{
     }
 
 }
+
+// keritav kuumade pakkumiste sektsioon
+//Swiperjs
+// Kood võetud lehelt https://swiperjs.com/demos alamlehtedelt "Responsive breakpoints" ning "Autoplay"
+let swiper = new Swiper(".vehicles_slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    grabCursor: true,
+    centeredSlides: true,
+    loop:true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        991: {
+            slidesPerView: 3
+        },
+    },
+});
